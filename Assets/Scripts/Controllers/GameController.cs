04c74bd4f5b17766d47dd8f83fb62aaeac181282
JackAlgera <jack.algera@live.com>
2018-10-24 18:09:49 +0200

    Added Arrow prefabs and background

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController instance;
+
+	void Start () {
+		if(instance == null)
+        {
+            instance = this;
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Sprites/ArrowController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+
+    public float speed;
+    private Animator anim;
+
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    public void Move()
+    {
+        transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnClickDestroy()
+    {
+        speed = 0;
+        anim.SetTrigger("Destroy");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "ArrowCollector")
+        {
+            OnClickDestroy();
+        }
+    }
+}
